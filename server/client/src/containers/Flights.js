d6a5b427cc1198151b5d5549c1fad05db87dfb1e
Tuba Kasap <codinglope@gmail.com>
2019-07-23 17:44:46 +0200

    first commit

--- server/client/src/containers/Flights.js
+++ server/client/src/containers/Flights.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
 import FlightForm from "../components/Flights/FlightForm"
+import Hero from "../components/hero/hero"
 
 export default class Flights extends Component {
   render() {
     return (
+
       <div>
+        <Hero
+          videosrc="https://pixabay.com/videos/download/video-1917_small.mp4"
+          h1="Flights"
+          h2="Search for flights"
+        />
         <FlightForm />
       </div>
     )
--- server/client/src/containers/Hotels.js
+++ server/client/src/containers/Hotels.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import Hero from "../components/hero/hero"
 
 export default class Hotels extends Component {
   render() {
     return (
       <div>
-        
+         <Hero
+          videosrc="https://pixabay.com/videos/download/video-24216_medium.mp4"
+          h1="Hotels"
+          h2="Search for Hotels"
+        />
       </div>
     )
   }
