9458905560c10d3fffe8b15d647a91284dcad41b
amerzbach <adi.merzbach@gmail.com>
2019-08-01 10:03:15 +0200

    done

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bda2cd161074be9eeb042d07d7e22887.js"
+  "/precache-manifest.16dd1aee8ec0ed8449fe101cce830e6f.js"
 );
 
 self.addEventListener('message', (event) => {
